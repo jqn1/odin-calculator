88ad9608907c167d63a2451411893a916c420c22
jqn1 <154387579+jqn1@users.noreply.github.com>
2024-08-22 18:14:22 -0400

    Calculator works
    Todo: deal with invalid operations, more than 1 operator

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const buttons = Array.from(document.querySelectorAll(".button"));
+const screen = document.querySelector(".screen");
+let displayContent = "";
+
+for (button of buttons){
+    button.addEventListener("click",(button) => {
+        buttonContent = button.originalTarget.textContent;
+        if (buttonContent === "C"){
+            displayContent = "";
+            screen.textContent = displayContent;
+        } else if (buttonContent === "="){
+            operation = splitOperation(displayContent);
+            result = operate(operation[2],operation[0],operation[1]);
+            console.log(result);
+            displayContent = result;
+            screen.textContent = displayContent;
+        } else {
+            displayContent += buttonContent;
+            screen.textContent = displayContent;
+        }
+    })
+}
+
+
 function add(a,b){
     return a+b;
 }
@@ -15,6 +39,8 @@ function divide(a, b) {
 }
 
 function operate(operator,a,b) {
+    a = Number(a);
+    b = Number(b);
     switch (operator) {
         case "+":
             return add(a,b);
@@ -25,5 +51,14 @@ function operate(operator,a,b) {
         case "*":
             return multiply(a,b);
     }
+
+}
+
+function splitOperation(screenContent){
+    let operator = screenContent.match(/[+\-*/]/);
+    let operation = screenContent.split(operator[0]);
+    operation.push(operator[0]);
+
+    return operation;
 
 }
