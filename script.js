c0874d9abb44e789e92b2004f09336517eaa1c4e
Joaquín Urrutia <154387579+jqn1@users.noreply.github.com>
2024-09-16 16:59:07 +0000

    Fixing negative numbers handling (wip)

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const buttons = Array.from(document.querySelectorAll(".button"));
 const screen = document.querySelector(".screen");
-let displayContent = "";
-
+let displayContent = ""
+let nextOperator = "";
 
 for (let button of buttons){
     button.addEventListener("click",(button) => {
@@ -15,7 +15,14 @@ for (let button of buttons){
             console.log(result);
             displayContent = result;
             screen.textContent = displayContent;
-        } else {
+        } else if((operatorAmount(displayContent) === 1)&&(["+","-","*","/"].includes(buttonContent))){
+            operation = splitOperation(displayContent);
+            result = operate(operation[2],operation[0],operation[1]);
+            displayContent = result+buttonContent;
+            screen.textContent = displayContent;
+
+        }
+        else {
             displayContent += buttonContent;
             screen.textContent = displayContent;
         }
@@ -56,10 +63,32 @@ function operate(operator,a,b) {
 }
 
 function splitOperation(screenContent){
-    let operator = screenContent.match(/[+\-*/]/);
-    let operation = screenContent.split(operator[0]);
-    operation.push(operator[0]);
+    //let operator = screenContent.match(/[+\-*/]/);
+   // let operation = screenContent.split(operator[0]);
+ //   operation.push(operator[0]);
+    let operation = [];
+    for (let i = 0;i <= screenContent.length; i++){
+        character = screenContent[i];
+        number = "";
+        if(["+","-","*","/"].includes(character)){
+            operation.push(number);
+            operation.push(character);
+            number = "";
+        } else{
+            number += character;
+        }
+
+
+    }
 
     return operation;
 
+}
+
+function operatorAmount(screenContent){ 
+    let operators = screenContent.match(/[+\-*/]/g);
+    if (operators === null){
+        return 0;
+    }
+    return operators.length;
 }
