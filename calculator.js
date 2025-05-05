ce543d6785f23d57f473f6971642901a17acbb85
jqn1 <154387579+jqn1@users.noreply.github.com>
2025-05-05 16:39:13 -0400

    Create Modules
    Decided to split files and turn calculator into a class to make it easier to maintain and create code

---
+++ calculator.js
@@ -0,0 +1,7 @@
+import {add, subtract, multiply, divide, operate} from "./operations.js"
+
+class Calculator {
+    constructor(){
+
+    }
+}
--- script.js
+++ script.js
@@ -1,71 +1,36 @@
+import {add, subtract, multiply, divide, operate} from "./operations.js"
+
 const buttons = Array.from(document.querySelectorAll(".button"));
 const screen = document.querySelector(".screen");
-let displayContent = ""
-let nextOperator = "";
+
+let displayContent = "";
+
 
 for (let button of buttons){
     button.addEventListener("click",(button) => {
-        buttonContent = button.target.textContent;
+        let buttonContent = button.target.textContent;
         if (buttonContent === "C"){
             displayContent = "";
-            screen.textContent = displayContent;
         } else if (buttonContent === "="){
             operation = splitOperation(displayContent);
             result = operate(operation[2],operation[0],operation[1]);
             console.log(result);
             displayContent = result;
-            screen.textContent = displayContent;
         } else if((operatorAmount(displayContent) === 1)&&(["+","-","*","/"].includes(buttonContent))){
             operation = splitOperation(displayContent);
             result = operate(operation[2],operation[0],operation[1]);
-            displayContent = result+buttonContent;
-            screen.textContent = displayContent;
+            displayContent = result + buttonContent;
 
         }
         else {
             displayContent += buttonContent;
             screen.textContent = displayContent;
         }
+        screen.textContent = displayContent;
     })
 }
 
-
-function add(a,b){
-    return a+b;
-}
-
-function subtract(a,b){
-    return a-b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
-function operate(operator,a,b) {
-    a = Number(a);
-    b = Number(b);
-    switch (operator) {
-        case "+":
-            return add(a,b);
-        case "-":
-            return subtract(a,b);
-        case "/":
-            return divide(a,b);
-        case "*":
-            return multiply(a,b);
-    }
-
-}
-
 function splitOperation(screenContent){
-    //let operator = screenContent.match(/[+\-*/]/);
-   // let operation = screenContent.split(operator[0]);
- //   operation.push(operator[0]);
     let operation = [];
     for (let i = 0;i <= screenContent.length; i++){
         character = screenContent[i];
